eda1e0d8649aadd038521088bf233bbb876401cf
dmmanns <dmmanns555@gmail.com>
2015-11-02 17:23:00 -0500

    test simon says completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,18 +1,59 @@
-def echo
+def echo(str)
+	str
 end
 
-def shout
+def shout(str)
+	str.upcase 
 end
 
-def repeat
+def repeat(string, num = 2)
+	# if num >= 3
+	# 	"#{string} #{string} #{string}"
+	# elsif num >= 1 
+	# 	"#{string} #{string}"
+	repeated_string = []
+	
+	num.times do |n|
+		repeated_string << string
+	end
+
+	repeated_string.join(' ')
 end
 
-def start_of_word
+def start_of_word(word, number)
+	word[0, number]
+
+	# letters = word.split("")
+	# counter = 0
+	# to_keep = []
+
+	# letters.each do |character|
+		
+	# 	if counter < number
+	# 	to_keep << character 
+	# 	else 
+	# 	  return to_keep.join
+	# 	end
+		
+	# 	counter += 1 
+	#end
+ 
 end
 
-def first_word
+def first_word(phrase)
+	phrase.split[0]
 end
 
-def titleize
+def titleize(title)
+	lower_case = ["and", "over", "the"]
+	capitalized = title.split.map do |word|
+		if lower_case.include? word
+			word
+		else 
+			word.capitalize
+		end 
+	end
+	capitalized[0] = capitalized[0].capitalize
+	capitalized.join(' ')
 end
 
